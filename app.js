8d9518deb1fe84db924bb9fd1369fed07a21e87d
Paulo Pires <taralloqueiroiz@gmail.com>
2014-04-24 23:33:14 -0300

    calling roger wrapper

--- app.js
+++ app.js
@@ -11,5 +11,8 @@ app.use(morgan('dev'));                           // log every request to the co
 app.use(bodyParser());                            // pull information from html in POST
 app.use(methodOverride());                        // simulate DELETE and PUT
 
+
+roger.openConn({public_key: '2f435d0d8c3bef196a3246d0e345d7d2', private_key: 'v17824cddf39bf747f98cdf7c9b1cefd272b60f94'});
+
 app.listen(3000);
 console.log('Magic happens on port ' + 3000);     // shoutout to the user
